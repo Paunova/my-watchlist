c79e728406413b3473107846dda5f53822ea8910
Paunova <misspaunova@gmail.com>
2020-05-14 17:17:07 +0200

    add: header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TITLE = 'My watchlist';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='title'>{TITLE}</div>
+        </div>
+    )
+};
+
+export default Header;
