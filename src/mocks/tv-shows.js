b1c06adb6c554e55d4483e7235ea49e83b43729a
Paunova <misspaunova@gmail.com>
2020-05-14 17:47:58 +0200

    add: tv show mocks

---
+++ src/mocks/tv-shows.js
@@ -0,0 +1,16 @@
+const tvShows = [
+    {
+        title: 'After Life',
+        imdbId: 'tt8398600',
+        netflixId: '80998491',
+        img: 'https://m.media-amazon.com/images/M/MV5BZjdjOWIxMDgtYTgwNS00MjE4LTliZWYtZGI1NDhhZmIyYjM1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    },
+    {
+        title: 'Sherlock',
+        imdbId: 'tt1475582',
+        netflixId: '70202589',
+        img: 'https://m.media-amazon.com/images/M/MV5BMWY3NTljMjEtYzRiMi00NWM2LTkzNjItZTVmZjE0MTdjMjJhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTQ4NTc5OTU@._V1_.jpg'
+    }
+];
+
+export default tvShows;
