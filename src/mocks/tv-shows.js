4f13e5032c2dba53315814b2a72d91f0c0876d74
Paunova <misspaunova@gmail.com>
2020-05-14 19:03:00 +0200

    add: tv show mocks

--- src/mocks/tv-shows.js
+++ src/mocks/tv-shows.js
@@ -1,4 +1,19 @@
+/*
+    {
+        title: '',
+        imdbId: '',
+        netflixId: '',
+        img: ''
+    }
+*/
+
 const tvShows = [
+    {
+        title: 'Abstract: The Art of Design',
+        imdbId: 'tt6333098',
+        netflixId: '80057883',
+        img: 'https://m.media-amazon.com/images/M/MV5BMTUyMDU4NzE0Nl5BMl5BanBnXkFtZTgwMzg0MDQyMTI@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    },
     {
         title: 'After Life',
         imdbId: 'tt8398600',
@@ -10,6 +25,12 @@ const tvShows = [
         imdbId: 'tt1475582',
         netflixId: '70202589',
         img: 'https://m.media-amazon.com/images/M/MV5BMWY3NTljMjEtYzRiMi00NWM2LTkzNjItZTVmZjE0MTdjMjJhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTQ4NTc5OTU@._V1_.jpg'
+    },
+    {
+        title: 'When Calls the Heart',
+        imdbId: 'tt2874692',
+        netflixId: '80036407',
+        img: 'https://m.media-amazon.com/images/M/MV5BNjUxNjQ5NjgwNV5BMl5BanBnXkFtZTgwMzQzMDA1ODE@._V1_UX182_CR0,0,182,268_AL_.jpg'
     }
 ];
 
